363380cb9f1a693537e3e1769570fcdeaa31020e
Pedro Germano <pedrogermano232@gmail.com>
2024-08-27 08:27:34 -0300

    modal dialog with radix start

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,25 @@ import {
 } from "./styles";
 import logo from "../../assets/logo.svg";
 
+import * as Dialog from "@radix-ui/react-dialog";
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="Logo ignite" />
-        <NewTransactionsButton>Nova Transação</NewTransactionsButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionsButton>Nova Transação</NewTransactionsButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
