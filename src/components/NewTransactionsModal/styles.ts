ceb1c5f7bf5d3d1212ed83394eb5e790ad1c9dc4
Pedro Germano <pedrogermano232@gmail.com>
2024-08-27 20:44:17 -0300

    chapter done

--- src/components/NewTransactionsModal/styles.ts
+++ src/components/NewTransactionsModal/styles.ts
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
   width: 100vw;
   height: 100vh;
   inset: 0;
-  background: #00000075;
+  background: rgba(0, 0, 0, 0.75);
 `;
 
 export const Content = styled(Dialog.Content)`
@@ -14,25 +15,31 @@ export const Content = styled(Dialog.Content)`
   border-radius: 6px;
   padding: 2.5rem 3rem;
   background: ${(props) => props.theme["gray-800"]};
+
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
   form {
     margin-top: 2rem;
+
     display: flex;
     flex-direction: column;
     gap: 1rem;
+
     input {
       border-radius: 6px;
       border: 0;
       background: ${(props) => props.theme["gray-900"]};
       color: ${(props) => props.theme["gray-300"]};
       padding: 1rem;
+
       &::placeholder {
         color: ${(props) => props.theme["gray-500"]};
       }
     }
+
     button[type="submit"] {
       height: 50px;
       border: 0;
@@ -43,7 +50,13 @@ export const Content = styled(Dialog.Content)`
       border-radius: 6px;
       margin-top: 1.25rem;
       cursor: pointer;
-      &:hover {
+
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background: ${(props) => props.theme["green-700"]};
         transition: background-color 0.2s;
       }
@@ -62,7 +75,7 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme["gray-500"]};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -73,7 +86,9 @@ interface TransactionTypeButtonProps {
   variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(
+  RadioGroup.Item
+)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
@@ -81,8 +96,8 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
   justify-content: center;
   gap: 0.5rem;
   border-radius: 6px;
-  border: 0;
   cursor: pointer;
+  border: 0;
   color: ${(props) => props.theme["gray-300"]};
 
   svg {
@@ -90,5 +105,22 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
       props.variant === "income"
         ? props.theme["green-300"]
         : props.theme["red-300"]};
+  }
+
+  &[data-state="unchecked"]:hover {
+    transition: background-color 0.2s;
+    background: ${(props) => props.theme["gray-600"]};
+  }
+
+  &[data-state="checked"] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `;
